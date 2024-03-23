7c681ca22b78f8e2597b9008fa741fc9cd3ca9a3
mtownsend509 <mgtownsend509@gmail.com>
2024-03-23 14:48:44 -0700

    image

--- chess/src/Pieces.js
+++ chess/src/Pieces.js
@@ -11,7 +11,7 @@ class Pawn extends Piece {
         super(color);
         this.type = 'pawn'
         if(this.color == 'black') {
-        this.backgroundImage = 'https://png.pngtree.com/png-vector/20210702/ourmid/pngtree-black-chess-pawn-png-image_3539520.jpg';
+        this.backgroundImage = './images/black pawn.jpg';
         } else {
             this.backgroundImage = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cleanpng.com%2Ffree%2Fwhite-pawn.html&psig=AOvVaw30Kin07X54H325wjwFNMfK&ust=1711314052938000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNC2xeOji4UDFQAAAAAdAAAAABAF'
         }
